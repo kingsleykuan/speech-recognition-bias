c1f0a84bbdecf01a9bc454f2d14c6bb6508e6181
Kingsley Kuan <kingsley@kingsleykuan.com>
2021-10-13 21:27:21 +0800

    Add CNN LSTM model for speech emotion recognition

---
+++ cnn_lstm_model.py
@@ -0,0 +1,165 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from base_model import BaseModel
+
+
+LFLB_DEFAULT_CONFIG = [
+    {
+        'input_channels': 1,
+        'output_channels': 64,
+        'conv_kernel_size': 3,
+        'conv_stride': 1,
+        'pooling_kernel_size': 2,
+        'pooling_stride': 2,
+    },
+    {
+        'input_channels': 64,
+        'output_channels': 64,
+        'conv_kernel_size': 3,
+        'conv_stride': 1,
+        'pooling_kernel_size': 4,
+        'pooling_stride': 4,
+    },
+    {
+        'input_channels': 64,
+        'output_channels': 128,
+        'conv_kernel_size': 3,
+        'conv_stride': 1,
+        'pooling_kernel_size': 4,
+        'pooling_stride': 4,
+    },
+    {
+        'input_channels': 128,
+        'output_channels': 128,
+        'conv_kernel_size': 3,
+        'conv_stride': 1,
+        'pooling_kernel_size': 4,
+        'pooling_stride': 4,
+    },
+]
+
+
+class LocalFeatureLearningBlock2D(nn.Module):
+    def __init__(
+            self,
+            input_channels,
+            output_channels,
+            conv_kernel_size,
+            conv_stride,
+            pooling_kernel_size,
+            pooling_stride,
+            **kwargs):
+        super(LocalFeatureLearningBlock2D, self).__init__()
+
+        self.conv = nn.Conv2d(
+            input_channels,
+            output_channels,
+            conv_kernel_size,
+            conv_stride,
+            padding='same',
+            bias=False)
+        self.batch_norm = nn.BatchNorm2d(output_channels)
+        self.activation = nn.ELU()
+        self.pooling = nn.MaxPool2d(pooling_kernel_size, pooling_stride)
+
+        self.init_parameters()
+
+    def init_parameters(self):
+        with torch.no_grad():
+            nn.init.kaiming_uniform_(self.conv.weight, nonlinearity='relu')
+
+    def reset_parameters(self):
+        with torch.no_grad():
+            self.conv.reset_parameters()
+            self.batch_norm.reset_parameters()
+            self.init_parameters()
+
+    def forward(self, features):
+        features = self.conv(features)
+        features = self.batch_norm(features)
+        features = self.activation(features)
+        features = self.pooling(features)
+        return features
+
+
+class CNNLSTM2DModel(BaseModel):
+    def __init__(
+            self,
+            lflb_config=None,
+            lstm_input_size=128,
+            lstm_output_size=256,
+            output_size=6,
+            **kwargs):
+        super(CNNLSTM2DModel, self).__init__()
+
+        if lflb_config is None:
+            lflb_config = LFLB_DEFAULT_CONFIG
+        self.lflb_config = lflb_config
+        self.lstm_input_size = lstm_input_size
+        self.lstm_output_size = lstm_output_size
+        self.output_size = output_size
+
+        self.batch_norm_input = nn.BatchNorm2d(1)
+        self.lflbs = nn.Sequential(*[
+            LocalFeatureLearningBlock2D(**config)
+            for config in self.lflb_config])
+        self.lstm = nn.LSTM(self.lstm_input_size, self.lstm_output_size)
+        self.fc = nn.Linear(self.lstm_output_size, self.output_size)
+
+        self.init_parameters()
+
+    def config(self):
+        config = {
+            'lflb_config': self.lflb_config,
+            'lstm_input_size': self.lstm_input_size,
+            'lstm_output_size': self.lstm_output_size,
+            'output_size': self.output_size,
+        }
+        return config
+
+    def init_parameters(self):
+        with torch.no_grad():
+            nn.init.kaiming_uniform_(self.fc.weight, nonlinearity='linear')
+            nn.init.constant_(self.fc.bias, 0)
+
+    def reset_parameters(self):
+        with torch.no_grad():
+            self.batch_norm_input.reset_parameters()
+
+            for lflb in self.lflbs:
+                lflb.reset_parameters()
+
+            self.lstm.reset_parameters()
+            self.fc.reset_parameters()
+
+            self.init_parameters()
+
+    def loss(self, features, labels):
+        loss = F.binary_cross_entropy_with_logits(features, labels)
+        return loss
+
+    def forward(self, features, labels=None, **kwargs):
+        features = torch.unsqueeze(features, dim=1)
+        features = self.batch_norm_input(features)
+
+        features = self.lflbs(features)
+
+        # Permute features into (length, batch, height, channels)
+        features = torch.permute(features, (3, 0, 2, 1))
+        features = torch.reshape(
+            features, (features.shape[0], features.shape[1], -1))
+        lstm_output, (hidden_state_n, cell_state_n) = self.lstm(features)
+
+        features = lstm_output[-1]
+        features = self.fc(features)
+
+        outputs = {
+            'logits': features,
+        }
+
+        if labels is not None:
+            outputs['loss'] = self.loss(features, labels)
+
+        return outputs
---
+++ train_cnn_lstm_model.py
@@ -0,0 +1,118 @@
+import torch
+from torch.utils.data import DataLoader
+
+from crema_data import CremaAudioDataset
+from ser_trainer import SpeechEmotionRecognitionTrainer
+from cnn_lstm_model import CNNLSTM2DModel
+
+data_path_train = 'Data/MelSpecSplit/train'
+data_path_val = 'Data/MelSpecSplit/val'
+demographics_csv_path = 'Data/VideoDemographics.csv'
+ratings_csv_path = 'Data/processedResults/summaryTable.csv'
+save_path = 'models/cnn_lstm'
+log_dir = 'runs/cnn_lstm'
+use_ratings = False
+num_epochs = 50
+steps_per_log = 50
+epochs_per_eval = 5
+batch_size = 64
+num_workers = 4
+learning_rate = 1e-3
+weight_decay = 1e-3
+random_seed = 0
+
+
+def load_data(
+        data_path,
+        demographics_csv_path,
+        ratings_csv_path,
+        batch_size=32,
+        num_workers=4,
+        shuffle=True):
+    dataset = CremaAudioDataset(
+        data_path, demographics_csv_path, ratings_csv_path)
+    data_loader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=shuffle,
+        num_workers=num_workers,
+        pin_memory=True,
+        prefetch_factor=2,
+        persistent_workers=True)
+    return data_loader
+
+
+def load_model():
+    model = CNNLSTM2DModel(output_size=6)
+    return model
+
+
+def main(
+        data_path_train,
+        data_path_val,
+        demographics_csv_path,
+        ratings_csv_path,
+        save_path,
+        log_dir=None,
+        use_ratings=False,
+        num_epochs=20,
+        steps_per_log=100,
+        epochs_per_eval=5,
+        batch_size=32,
+        num_workers=4,
+        learning_rate=1e-3,
+        weight_decay=1e-5,
+        random_seed=0):
+    torch.manual_seed(random_seed)
+
+    data_loader_train = load_data(
+        data_path_train,
+        demographics_csv_path,
+        ratings_csv_path,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=True)
+
+    data_loader_eval = load_data(
+        data_path_val,
+        demographics_csv_path,
+        ratings_csv_path,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        shuffle=False)
+
+    model = load_model()
+
+    trainer = SpeechEmotionRecognitionTrainer(
+        data_loader_train,
+        data_loader_eval,
+        model,
+        num_epochs=num_epochs,
+        steps_per_log=steps_per_log,
+        epochs_per_eval=epochs_per_eval,
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+        log_dir=log_dir,
+        save_path=save_path,
+        use_ratings=use_ratings)
+
+    trainer.train()
+
+
+if __name__ == '__main__':
+    main(
+        data_path_train,
+        data_path_val,
+        demographics_csv_path,
+        ratings_csv_path,
+        save_path,
+        log_dir=log_dir,
+        use_ratings=use_ratings,
+        num_epochs=num_epochs,
+        steps_per_log=steps_per_log,
+        epochs_per_eval=epochs_per_eval,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        learning_rate=learning_rate,
+        weight_decay=weight_decay,
+        random_seed=random_seed)
