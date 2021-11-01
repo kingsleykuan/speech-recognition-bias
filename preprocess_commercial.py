37473c06be01e3d9794766affe35d6d038152043
Wei Kang Wong <wwk97@hotmail.sg>
2021-11-02 03:55:02 +0800

    Evaluate User Models & Preprocess Commercial Results

--- preprocess_commercial.py
+++ preprocess_commercial.py
@@ -2,12 +2,6 @@
 Preprocess Commercial .json results
 """
 from pathlib import Path
-from sklearn.metrics import classification_report, f1_score
-import numpy as np
-import os
-import pandas as pd
-from pathlib import Path
-from crema_metadata import read_actor_demographics, read_ratings
 import json
 import pandas as pd
 
@@ -44,7 +38,96 @@ def preprocess_vokaturi(path):
     return df     
 
 def preprocess_empath(path):
-    pass
+    data_path = Path(path)
+    paths = [path for path in data_path.glob('**/*') if path.is_file()]
+    index = []
+    anger = []
+    happy = []
+    neutral = []
+    sad = []
+    
+    # empath
+    for path in paths:
+        data = json.load(open(path))
+        ind = data['index']
+        preds = data['empath']
+        print(preds)
+        
+        index.append(ind)
+        anger.append(preds['anger'])
+        happy.append(preds['joy'])
+        neutral.append(preds['calm'])
+        sad.append(preds['sorrow'])
+    
+    df = pd.DataFrame(data=zip(index,anger,happy,neutral,sad), \
+                      columns=['Filename','Anger','Happy', 'Neutral', 'Sad'])
+    return df    
+
     
 def preprocess_da(path):
-    pass
+    data_path = Path(path)
+    paths = [path for path in data_path.glob('**/*') if path.is_file()]
+    index = []
+    anger = []
+    happy = []
+    neutral = []
+    sad = []
+    
+    # deepaffect
+    for path in paths:
+        data = json.load(open(path))
+        ind = data['index']
+        preds_arr = data['deepaffect']
+        print(preds_arr)
+        
+        index.append(ind)
+
+        if len(preds_arr) > 1:
+            timing = []
+            emotion = []
+            for preds in preds_arr:
+                timing.append(preds['end'] - preds['start'])
+                emotion.append(preds['emotion'])
+            # timing= [3, 0.1]
+            # emotion= ['excited', 'neutral']
+
+            max_index =  timing.index(max(timing))
+            emotion_pred = emotion[max_index]
+        elif len(preds_arr) == 1:
+            emotion_pred = preds_arr[0]['emotion']
+
+
+        if emotion_pred == 'anger' or emotion_pred == 'frustration':
+            anger.append(1)
+            happy.append(0)
+            neutral.append(0)
+            sad.append(0)
+        if emotion_pred == 'happy' or emotion_pred == 'excited':
+            anger.append(0)
+            happy.append(1)
+            neutral.append(0)
+            sad.append(0)
+        if emotion_pred == 'neutral':
+            anger.append(0)
+            happy.append(0)
+            neutral.append(1)
+            sad.append(0)
+        if emotion_pred == 'sad':
+            anger.append(0)
+            happy.append(0)
+            neutral.append(0)
+            sad.append(1)
+        
+
+    
+    df = pd.DataFrame(data=zip(index,anger,happy,neutral,sad), \
+                      columns=['Filename','Anger','Happy', 'Neutral', 'Sad'])
+    return df
+
+
+if __name__ == '__main__': 
+    preprocess_da("predictions/commercial_results/results").to_csv('predictions/commercial_results/deepaffect.csv', index = False)
+    preprocess_empath("predictions/commercial_results/results").to_csv('predictions/commercial_results/empath.csv', index = False)
+    preprocess_vokaturi('predictions/commercial_results/results').to_csv('predictions/commercial_results/vokaturi.csv', index = False)
+
+
