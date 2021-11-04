09f53347eaeb9fc42b686708af98e20c0ff797dc
Kingsley Kuan <kingsley@kingsleykuan.com>
2021-11-05 06:03:24 +0800

    Refactor SVM preprocessing

--- preprocess_svm_data.py
+++ preprocess_svm_data.py
@@ -1,113 +1,18 @@
-import csv
-import os
+import argparse
 import pickle
+from pathlib import Path
 
 import numpy as np
+from tqdm import tqdm
 
 from AudioLibrary.AudioFeatures import AudioFeatures
 from AudioLibrary.AudioSignal import AudioSignal
-
-csv_path = "Data/processedResults/summaryTable.csv"
-name = open(csv_path, 'r')
-file = csv.DictReader(name)
-
-filename_voicevote = []
-
-for col in file:
-    filename = col['FileName'] + '.wav'
-    voicevote = col['VoiceVote']
-    filename_voicevote.append([filename, voicevote])
-
-file_path = 'Data/AudioWAVSplit/train/'
-files = set(os.listdir(file_path))
-
-filename_voicevote = [
-    (filename, voicevote)
-    for (filename, voicevote) in filename_voicevote
-    if filename in files]
-
-print(filename_voicevote)
-
-# Start feature extraction
-print("Import Data: START")
+from crema_metadata import read_metadata, read_ratings
 
 # Sample rate (44.1 kHz)
-sample_rate = 44100
+SAMPLE_RATE = 44100
 
-# Initialize signal and labels list
-signal = []
-labels = []
-
-# Compute global statistics features for all audio file
-for audio_index, (audio_file, voicevote) in enumerate(filename_voicevote):
-    # Read audio file
-    signal.append(AudioSignal(sample_rate, filename=file_path + audio_file))
-
-    # Set label
-    labels.append(voicevote)
-
-    # Print running...
-    if (audio_index % 100 == 0):
-        print("Import Data: RUNNING ... {} files".format(audio_index))
-        break
-
-# Cast labels to array
-labels = np.asarray(labels).ravel()
-
-# Stop feature extraction
-print("Import Data: END \n")
-print("Number of audio files imported: {}".format(labels.shape[0]))
-
-
-# Audio features extraction function
-def global_feature_statistics(
-        y, win_size=0.025, win_step=0.01, nb_mfcc=12, mel_filter=40,
-        stats = ['mean', 'std', 'med', 'kurt', 'skew', 'q1', 'q99', 'min', 'max', 'range'],
-        features_list =  ['zcr', 'energy', 'energy_entropy', 'spectral_centroid', 'spectral_spread', 'spectral_entropy', 'spectral_flux', 'sprectral_rolloff', 'mfcc']):
-
-    # Extract features
-    audio_features = AudioFeatures(y, win_size, win_step)
-    features, features_names = audio_features.global_feature_extraction(stats=stats, features_list=features_list)
-    return features
-
-
-# Features extraction parameters
-sample_rate = 16000 # Sample rate (16.0 kHz)
-win_size = 0.025    # Short term window size (25 msec)
-win_step = 0.01     # Short term window step (10 msec)
-nb_mfcc = 12        # Number of MFCCs coefficients (12)
-nb_filter = 40      # Number of filter banks (40)
-stats = ['mean', 'std', 'med', 'kurt', 'skew', 'q1', 'q99', 'min', 'max', 'range'] # Global statistics
-features_list =  ['zcr', 'energy', 'energy_entropy', 'spectral_centroid', 'spectral_spread', # Audio features
-                  'spectral_entropy', 'spectral_flux', 'sprectral_rolloff', 'mfcc']
-
-print(global_feature_statistics(signal[0]))
-
-# Start feature extraction
-print("Feature extraction: START")
-
-# Compute global feature statistics for all audio file
-features = np.asarray(list(map(global_feature_statistics, signal)))
-
-# Stop feature extraction
-print("Feature extraction: END!")
-
-# Anger (ANG) 1
-# Disgust (DIS) 2
-# Fear (FEA) 3
-# Happy/Joy (HAP) 4
-# Neutral (NEU) 5 
-# Sad (SAD) 6
-
-# Happy 4
-# Neutral 5
-
-# label = 'H:N'
-# labels = [0, 0, 0, 1, 1, 0]
-
-print(labels)
-
-emotions = {
+EMOTIONS = {
     'A': 0,
     'D': 1,
     'F': 2,
@@ -116,16 +21,118 @@ emotions = {
     'S': 5,
 }
 
-label_vectors = []
-for label in labels:
-    label_ids = np.zeros(len(emotions))
-    for l in label.split(':'):
-        label_id = emotions[l]
-        label_ids[label_id] = 1
-    label_vectors.append(label_ids)
 
-label_vectors = np.asarray(label_vectors)
-print(label_vectors)
+def arg_parser():
+    parser = argparse.ArgumentParser(
+        description="Preprocess SVM data.")
 
-# Save DataFrame to pickle
-pickle.dump([features, label_vectors], open("Data/SVM_features/observe_preprocessing_train.p", 'wb'))
+    parser.add_argument(
+        '--data_path', default='Data/AudioWAVSplit/test', type=str,
+        help='Path to audio wav files.')
+
+    parser.add_argument(
+        '--ratings_path', default='Data/processedResults/summaryTable.csv',
+        type=str, help='Path ratings CSV file.')
+
+    parser.add_argument(
+        '--output_path', default='Data/svm_features/test.p', type=str,
+        help='Path to output SVM features.')
+
+    return parser
+
+
+# Audio features extraction function
+def global_feature_statistics(
+        y,
+        win_size=0.025,
+        win_step=0.01,
+        nb_mfcc=12,
+        mel_filter=40,
+        stats=[
+            'mean',
+            'std',
+            'med',
+            'kurt',
+            'skew',
+            'q1',
+            'q99',
+            'min',
+            'max',
+            'range'
+        ],
+        features_list=[
+            'zcr',
+            'energy',
+            'energy_entropy',
+            'spectral_centroid',
+            'spectral_spread',
+            'spectral_entropy',
+            'spectral_flux',
+            'sprectral_rolloff',
+            'mfcc'
+        ]):
+    # Extract features
+    audio_features = AudioFeatures(y, win_size, win_step)
+    features, features_names = audio_features.global_feature_extraction(
+        stats=stats,
+        features_list=features_list,
+        nb_mfcc=nb_mfcc,
+        nb_filter=mel_filter)
+    return features
+
+
+def main(data_path, ratings_path, output_path):
+    print("Import Data: START")
+
+    metadata = read_metadata(data_path)
+    ratings = read_ratings(ratings_path)
+
+    metadata = metadata.filter(items=['path', 'filename', 'emotion'])
+    ratings = ratings.filter(items=['filename', 'voice_vote'])
+
+    metadata = metadata.merge(ratings, how='inner', on='filename', sort=True)
+
+    signals = []
+    for path in tqdm(metadata['path']):
+        # Read audio file
+        signals.append(AudioSignal(SAMPLE_RATE, filename=path))
+
+    print("Import Data: END")
+    print("Number of audio files imported: {}".format(len(signals)))
+
+    print("Feature extraction: START")
+    features = [global_feature_statistics(signal) for signal in tqdm(signals)]
+    print("Feature extraction: END!")
+
+    # Create label vectors
+    intended_label_vectors = np.zeros((len(metadata), len(EMOTIONS)))
+    for i, label in enumerate(metadata['emotion']):
+        label_id = EMOTIONS[label]
+        intended_label_vectors[i, label_id] = 1
+
+    observed_label_vectors = np.zeros((len(metadata), len(EMOTIONS)))
+    for i, observed_label in enumerate(metadata['voice_vote']):
+        for label in observed_label.split(':'):
+            label_id = EMOTIONS[label]
+            observed_label_vectors[i, label_id] = 1
+
+    # Save DataFrame to pickle
+    filenames = metadata['filename'].tolist()
+
+    output_path = Path(output_path)
+    output_path.parent.mkdir(parents=True, exist_ok=True)
+    with open(output_path, 'wb') as file:
+        pickle.dump(
+            {
+                'filenames': filenames,
+                'features': features,
+                'intended_label_vectors': intended_label_vectors,
+                'observed_label_vectors': observed_label_vectors
+            },
+            file)
+
+
+if __name__ == '__main__':
+    parser = arg_parser()
+    args = parser.parse_args()
+    main(args.data_path, args.ratings_path, args.output_path)
